6f0a1cd9abb08132ba0b1711d5436b8aeff65715
JangSeunghyun <jang980918@gmail.com>
2023-01-10 10:50:43 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,35 @@
 #!/bin/sh     
-sudo git pull origin master
-sudo pip3 install -r requirements.txt
+# sudo git pull origin master
+# sudo pip3 install -r requirements.txt
+
+echo "hello!"
+
+cd $(dirname "$0")
+pwd //현재경로
+
+// sh파일이 다른 폴더에 있다면 cd 명령어를 통해 root로 와야한다.
+
+echo "activate venv..."
+source team7elog/bin/activate
+
+echo "install package..."
+pip install -r "requirements.txt"
+
+echo "collect static..."
+python manage.py collectstatic
+
+echo "make migrations..."
 python3 manage.py makemigrations
+
+echo "apply migration..."
 python3 manage.py migrate
-python3 manage.py collectstatic
-sudo systemctl restart nginx
-sudo systemctl restart gunicorn
+
+echo "move location to project..."
+cd test/team7-server/velog
+
+echo "gunicorn..."
+nohup gunicorn velog.wsgi:application -b 0.0.0.0:8080
+
+# sudo systemctl restart nginx
+# sudo systemctl restart gunicorn
+
