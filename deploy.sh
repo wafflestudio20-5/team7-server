736eb3d425b672c0acab5d4ce54981bdb8e4bcdc
JangSeunghyun <jang980918@gmail.com>
2023-01-10 11:10:10 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ echo "move location to project..."
 cd test/team7-server/velog
 
 echo "gunicorn..."
-gunicorn velog.wsgi:application -b 0.0.0.0:8080
+nohup gunicorn velog.wsgi:application -b 0.0.0.0:8080
 
 # sudo systemctl restart nginx
 # sudo systemctl restart gunicorn
