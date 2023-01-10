c92d6dadea5fd9255a15e80e835e0e03ee6ec4b3
JangSeunghyun <jang980918@gmail.com>
2023-01-10 17:29:54 +0900

    Update asgi.py

--- velog/velog/asgi.py
+++ velog/velog/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 
 sys.path.append('/velog')
 
-sys.path.append('~/team7elog/lib/python3.8/site-packages')
+sys.path.append('/home/ubuntu/team7elog/lib/python3.8/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'velog.settings')
 
