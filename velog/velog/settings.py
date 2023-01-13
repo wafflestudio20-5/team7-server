aaed9ea7ff1b3a4cfd51ed07e8bfd1fde60485c6
JangSeunghyun <jang980918@gmail.com>
2023-01-14 03:04:47 +0900

    Update settings.py

--- velog/velog/settings.py
+++ velog/velog/settings.py
@@ -237,12 +237,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = '/home/ubuntu/velog/velog/static'
+STATIC_ROOT = '/home/ubuntu/team7elog/lib/python3.8/site-packages/rest_framework/'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
