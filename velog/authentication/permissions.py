a02729649946a982df1bebb730353112106b526d
JangSeunghyun <jang980918@gmail.com>
2023-02-03 01:52:08 +0900

    Update permissions.py

--- velog/authentication/permissions.py
+++ velog/authentication/permissions.py
@@ -4,3 +4,12 @@ from rest_framework import permissions
 class IsCreator(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj == request.user
+    
+from .models import User
+
+class IsAccountOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj: User):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj == request.user
