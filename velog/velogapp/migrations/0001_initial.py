3b42abdd7a5282385c0cc19ba109c7f8b581e3ee
JangSeunghyun <jang980918@gmail.com>
2023-02-02 20:18:55 +0900

    Update 0001_initial.py

--- velog/velogapp/migrations/0001_initial.py
+++ velog/velogapp/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Tag',
+            name='Tag',            
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('tag_name', models.CharField(max_length=200)),
