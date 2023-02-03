f3d6c1668eaa8e513c2c1ae0a9a8dd761f0cecef
JangSeunghyun <jang980918@gmail.com>
2023-02-03 09:08:07 +0900

    Update settings.py

--- velog/velog/settings.py
+++ velog/velog/settings.py
@@ -138,8 +138,8 @@ REST_FRAMEWORK = {
     "NON_FIELD_ERRORS_KEY": "errors",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+#         'rest_framework.authentication.BasicAuthentication',
+#         'rest_framework.authentication.TokenAuthentication',
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
