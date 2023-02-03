3b4a2862d1f157dced28c1668d89afe2cd187a6b
JangSeunghyun <jang980918@gmail.com>
2023-02-04 01:04:02 +0900

    Update 0001_initial.py

--- velog/authentication/migrations/0001_initial.py
+++ velog/authentication/migrations/0001_initial.py
@@ -36,5 +36,4 @@ class Migration(migrations.Migration):
                 'abstract': False,
             },
         ),
-    ]
-~                                 
+    ]                                
